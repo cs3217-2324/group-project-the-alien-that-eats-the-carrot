93d4fdf375decd800bd2b300b903d6751212c0a7
Echomo Xinyu <seanxinyu2@gmail.com>
2024-03-12 19:37:15 +0800

    add utilities for CoreGraphics

---
+++ TheAlienThatEatsTheCarrot/Model/Utilities/CGFloat+extension.swift
@@ -0,0 +1,14 @@
+//
+//  CGFloat+extension.swift
+//  TheAlienThatEatsTheCarrot
+//
+//  Created by Sun Xinyu on 12/3/24.
+//
+
+import Foundation
+
+extension CGFloat {
+    func square() ->CGFloat {
+        self * self
+    }
+}
