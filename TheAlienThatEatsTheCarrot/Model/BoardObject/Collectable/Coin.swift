4dbca1c41edfbb2f304b38f466763858805184c9
Echomo Xinyu <seanxinyu2@gmail.com>
2024-03-12 20:26:13 +0800

    add collectable

--- TheAlienThatEatsTheCarrot/Model/BoardObject/Collectable/Coin.swift
+++ TheAlienThatEatsTheCarrot/Model/BoardObject/Collectable/Coin.swift
@@ -5,4 +5,15 @@
 //  Created by Sun Xinyu on 12/3/24.
 //
 
-import Foundation
+import CoreGraphics
+
+class Coin: Collectable {
+    static let COIN_COLLECTABLE_WIDTH: CGFloat = 30.0
+    static let COIN_COLLECTABLE_HEIGHT: CGFloat = 30.0
+    // TODO: update when adding asset
+    static let imageName = ""
+
+    init(position: CGPoint = .zero, width: CGFloat = Coin.COIN_COLLECTABLE_WIDTH, height: CGFloat = Coin.COIN_COLLECTABLE_HEIGHT) {
+        super.init(imageName: Coin.imageName, width: width, height: height, position: position)
+    }
+}
