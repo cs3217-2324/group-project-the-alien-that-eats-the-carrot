550689a113bc58a8bbe1f63858c86d5442cbb80a
Echomo Xinyu <seanxinyu2@gmail.com>
2024-03-14 12:37:00 +0800

    add PhysicsBody

---
+++ TheAlienThatEatsTheCarrot/PhysicsEngine/PhysicsBody.swift
@@ -0,0 +1,77 @@
+//
+//  PhysicsBody.swift
+//  TheAlienThatEatsTheCarrot
+//
+//  Created by Sun Xinyu on 14/3/24.
+//
+
+import CoreGraphics
+
+final class PhysicsBody {
+    // MARK: Physical properties
+    var mass: CGFloat
+    var rotation: Double
+    var shape: Shape
+    var size: CGSize
+    var velocity: CGVector
+    var restitution: CGFloat
+    var position: CGPoint
+    
+    // MARK: Force related properties
+    var isTrigger: Bool
+    var isDynamic: Bool
+    var forces: [CGVector]
+    var netForce: CGVector {
+        forces.reduce(CGVector.zero, +)
+    }
+
+    init(shape: Shape,
+         position: CGPoint,
+         size: CGSize,
+         isDynamic: Bool,
+         rotation: CGFloat = .zero,
+         mass: CGFloat = 1,
+         velocity: CGVector = .zero,
+         forces: [CGVector] = [],
+         restitution: CGFloat = PhysicsConstants.restitution,
+         isTrigger: Bool = false) {
+        self.shape = shape
+        self.position = position
+        self.size = CGSize(width: max(size.width, PhysicsConstants.physicsBodyMinimumSize),
+                           height: max(size.height, PhysicsConstants.physicsBodyMinimumSize))
+        self.rotation = rotation
+        self.mass = mass
+        self.velocity = velocity
+        self.forces = forces
+        self.restitution = restitution
+        self.isDynamic = isDynamic
+        self.isTrigger = isTrigger
+    }
+
+    func applyForce(_ force: CGVector) {
+        forces.append(force)
+    }
+
+    func update(deltaTime: CGFloat) {
+        let maxSpeed = PhysicsConstants.maxSpeed
+        velocity += netForce / mass * deltaTime
+        if velocity.magnitude > maxSpeed {
+            velocity = velocity.unitVector * maxSpeed
+        }
+
+        let vector = velocity * deltaTime
+        position += vector
+
+        forces = []
+    }
+}
+
+extension PhysicsBody: Hashable {
+    static func == (lhs: PhysicsBody, rhs: PhysicsBody) -> Bool {
+        lhs === rhs
+    }
+
+    func hash(into hasher: inout Hasher) {
+        hasher.combine(ObjectIdentifier(self))
+    }
+}
---
+++ TheAlienThatEatsTheCarrot/PhysicsEngine/PhysicsConstants.swift
@@ -0,0 +1,14 @@
+//
+//  PhysicsConstants.swift
+//  TheAlienThatEatsTheCarrot
+//
+//  Created by Sun Xinyu on 14/3/24.
+//
+
+import CoreGraphics
+
+class PhysicsConstants {
+    static let physicsBodyMinimumSize: CGFloat = 0.05
+    static let restitution: CGFloat = 0.8
+    static let maxSpeed: CGFloat = 0.6
+}
