98e501c9e9c2ef035b1577afec4b64dbdd38ee75
JCSnap <e0970167@u.nus.edu>
2024-03-15 13:57:34 +0800

    add game renderer

--- TheAlienThatEatsTheCarrot/GameRenderer/GameRenderer.swift
+++ TheAlienThatEatsTheCarrot/GameRenderer/GameRenderer.swift
@@ -6,3 +6,34 @@
 //
 
 import Foundation
+import QuartzCore
+
+class GameRenderer {
+    private var displayLink: CADisplayLink?
+    var gameEngine: GameEngine?
+
+    func start() {
+        self.displayLink = createDisplayLink()
+    }
+
+    func stop() {
+        displayLink?.remove(from: .main, forMode: .default)
+        displayLink?.invalidate()
+    }
+
+    func createDisplayLink() -> CADisplayLink {
+        let displaylink = CADisplayLink(target: self,
+                                        selector: #selector(step))
+        displaylink.add(to: .current, forMode: .default)
+        return displaylink
+    }
+
+    @objc func step(displaylink: CADisplayLink) {
+        if gameEngine == nil {
+            return
+        }
+        let timeInterval = displaylink.targetTimestamp - displaylink.timestamp
+        print("\(timeInterval)")
+        gameEngine?.update(deltaTime: timeInterval)
+    }
+}
