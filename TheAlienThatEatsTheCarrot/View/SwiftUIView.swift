cfc899b26741dedad2e60ef22aafca0a007307ef
JCSnap <e0970167@u.nus.edu>
2024-03-15 14:15:19 +0800

    add sample file

---
+++ TheAlienThatEatsTheCarrot/View/SwiftUIView.swift
@@ -0,0 +1,18 @@
+//
+//  SwiftUIView.swift
+//  
+//
+//  Created by Justin Cheah Yun Fei on 15/3/24.
+//
+
+import SwiftUI
+
+struct SwiftUIView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    SwiftUIView()
+}
