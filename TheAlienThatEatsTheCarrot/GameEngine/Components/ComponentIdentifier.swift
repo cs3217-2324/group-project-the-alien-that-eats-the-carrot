d91b64e8c20fa533f4a36e91fbdcbbce748ddeef
Echomo Xinyu <seanxinyu2@gmail.com>
2024-03-20 16:35:10 +0800

    fix pbxproj meta data

--- TheAlienThatEatsTheCarrot/GameEngine/Components/ComponentIdentifier.swift
+++ TheAlienThatEatsTheCarrot/GameEngine/Components/ComponentIdentifier.swift
@@ -9,7 +9,7 @@ import Foundation
 
 struct ComponentIdentifier: Hashable {
     private let id: Int
-    
+
     init<T: Component>(_ componentType: T.Type) {
         self.id = ObjectIdentifier(componentType).hashValue
     }
