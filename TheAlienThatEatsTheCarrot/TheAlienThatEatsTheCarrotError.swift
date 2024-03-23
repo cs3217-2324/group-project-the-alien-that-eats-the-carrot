364bf1a09c1bc88962d04683b564aa6b3b4c548d
Echomo Xinyu <seanxinyu2@gmail.com>
2024-03-23 21:45:34 +0800

    add level coredata

--- TheAlienThatEatsTheCarrot/TheAlienThatEatsTheCarrotError.swift
+++ TheAlienThatEatsTheCarrot/TheAlienThatEatsTheCarrotError.swift
@@ -9,11 +9,14 @@ import Foundation
 
 enum TheAlienThatEatsTheCarrotError: Error {
     case invalidObjectTypeDataError(typeName: String?)
+    case invalidNSSetDataError
 
     var errorMessage: String? {
         switch self {
         case .invalidObjectTypeDataError(let typeName):
             return "'\(String(describing: typeName))' is an invalid block type"
+        case .invalidNSSetDataError:
+            return "Persistence data is invalid"
         }
     }
 }
