6e3706403a1e9965da30e7d2f50239f7f9b257d9
Echomo Xinyu <seanxinyu2@gmail.com>
2024-03-23 13:52:13 +0800

    make relevant adjustments after refactor

--- TheAlienThatEatsTheCarrot/Model/BoardObject/ObjectType.swift
+++ TheAlienThatEatsTheCarrot/Model/BoardObject/ObjectType.swift
@@ -13,24 +13,4 @@ public enum ObjectType {
     case collectable(CollectableType)
     case powerup(PowerupType)
     case character(CharacterType)
-
-    public enum EnemyType {
-        case normal, fast, stationary, turret
-    }
-
-    public enum BlockType {
-        case normal, ground, spike, breakable, pushable, powerup
-    }
-
-    public enum CollectableType {
-        case coin, carrot, heart
-    }
-
-    public enum PowerupType {
-        case invinsible, strength, attack, doubleJump
-    }
-
-    public enum CharacterType {
-        case normal
-    }
 }
