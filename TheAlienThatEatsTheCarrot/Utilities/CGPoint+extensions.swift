35adbdbe0af558a01040f85a09990db7cccc48fc
Echomo Xinyu <seanxinyu2@gmail.com>
2024-03-23 16:38:14 +0800

    add CGRect for CoreData and organize utilities

--- TheAlienThatEatsTheCarrot/Utilities/CGPoint+extensions.swift
+++ TheAlienThatEatsTheCarrot/Utilities/CGPoint+extensions.swift
@@ -1,10 +1,11 @@
 //
-//  CGPoint+extension.swift
+//  CGPoint+extensions.swift
 //  TheAlienThatEatsTheCarrot
 //
-//  Created by Sun Xinyu on 12/3/24.
+//  Created by Sun Xinyu on 23/3/24.
 //
 
+import CoreData
 import CoreGraphics
 
 extension CGPoint {
@@ -51,5 +52,20 @@ extension CGPoint {
             .concatenating(translateOriginToPivot)
 
         return self.applying(rotateAroundPivot)
+    }
+}
+
+extension CGPoint: FromDataAble {
+    init(data: CGPointData) throws {
+        self.init(x: data.x, y: data.y)
+    }
+}
+
+extension CGPoint: ToDataAble {
+    func toData(context: NSManagedObjectContext) -> NSManagedObject {
+        let pointData = CGPointData(context: context)
+        pointData.x = x
+        pointData.y = y
+        return pointData as NSManagedObject
     }
 }
---
+++ TheAlienThatEatsTheCarrot/Utilities/CGRect+extensions.swift
@@ -0,0 +1,31 @@
+//
+//  CGRect+extensions.swift
+//  TheAlienThatEatsTheCarrot
+//
+//  Created by Sun Xinyu on 23/3/24.
+//
+
+import CoreData
+import CoreGraphics
+
+extension CGRect: FromDataAble {
+    init(data: CGRectData) throws {
+        guard let positionData = data.position else {
+            throw TheAlienThatEatsTheCarrotError.invalidObjectTypeDataError(typeName: "CGRect")
+        }
+        let height = data.height
+        let width = data.width
+        let position = try CGPoint(data: positionData)
+        self.init(x: position.x, y: position.y, width: width, height: height)
+    }
+}
+
+extension CGRect: ToDataAble {
+    func toData(context: NSManagedObjectContext) -> NSManagedObject {
+        let rectData = CGRectData(context: context)
+        rectData.position = self.origin.toData(context: context) as? CGPointData
+        rectData.height = self.height
+        rectData.width = self.width
+        return rectData as NSManagedObject
+    }
+}
