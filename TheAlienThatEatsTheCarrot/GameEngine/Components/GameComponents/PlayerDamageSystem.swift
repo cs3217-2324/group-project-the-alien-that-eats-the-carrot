a180ca397efec5a9e9038f036f135ed7748be31e
JCSnap <e0970167@u.nus.edu>
2024-03-31 00:13:02 +0800

    implement basic player damage system

--- TheAlienThatEatsTheCarrot/GameEngine/Components/GameComponents/PlayerDamageSystem.swift
+++ TheAlienThatEatsTheCarrot/GameEngine/Components/GameComponents/PlayerDamageSystem.swift
@@ -8,7 +8,7 @@
 import Foundation
 
 /// Responsible to handling damage to players by blocks / enemies
-class PlayerDamageSystem: System {
+class PlayerDamageSystem: System, AttackableDelegate {
     var nexus: Nexus
 
     init(nexus: Nexus) {
@@ -16,5 +16,40 @@ class PlayerDamageSystem: System {
     }
 
     func update(deltaTime: CGFloat) {
+        damagePlayersWithBlocks()
+        damagePlayersWithEnemies()
     }
+
+    private func damagePlayersWithBlocks() {
+        let attackableBlockEntities = nexus.getEntities(withComponentTypes: [BlockComponent.self, AttackableComponent.self])
+        let playerComponents = nexus.getComponents(of: PlayerComponent.self)
+        for attackableBlockEntity in attackableBlockEntities {
+            guard
+                let attackableComponent = nexus.getComponent(of: AttackableComponent.self, for: attackableBlockEntity) else {
+                return
+            }
+            for playerComponent in playerComponents {
+                attackableComponent.attackIfPossible(attackee: playerComponent.entity, delegate: self)
+            }
+        }
+    }
+
+    private func damagePlayersWithEnemies() {
+        let attackableEnemyEntities = nexus.getEntities(withComponentTypes: [EnemyComponent.self, AttackableComponent.self])
+        let playerComponents = nexus.getComponents(of: PlayerComponent.self)
+        for attackableEnemyEntity in attackableEnemyEntities {
+            guard
+                let attackableComponent = nexus.getComponent(of: AttackableComponent.self, for: attackableEnemyEntity) else {
+                return
+            }
+            for playerComponent in playerComponents {
+                attackableComponent.attackIfPossible(attackee: playerComponent.entity, delegate: self)
+            }
+        }
+    }
+
+    func getComponent<T>(of type: T.Type, for entity: Entity) -> T? where T: Component {
+        nexus.getComponent(of: type, for: entity)
+    }
+    
 }
