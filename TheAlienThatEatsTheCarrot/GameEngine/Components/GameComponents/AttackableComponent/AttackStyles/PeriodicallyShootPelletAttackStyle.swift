d6f8d6119c60b03c5f24ae24fb96725399e2e30e
JCSnap <e0970167@u.nus.edu>
2024-04-10 14:06:09 +0800

    add shoot projectile attack style

---
+++ TheAlienThatEatsTheCarrot/GameEngine/Components/GameComponents/AttackableComponent/AttackStyles/PeriodicallyShootPelletAttackStyle.swift
@@ -0,0 +1,54 @@
+//
+//  PeriodicallyShootPelletAttackStyle.swift
+//  TheAlienThatEatsTheCarrot
+//
+//  Created by Justin Cheah Yun Fei on 10/4/24.
+//
+
+import Foundation
+
+class PeriodicallyShootPelletAttackStyle: AttackStyle {
+    static let DEFAULT_PELLET_SPEED = 50.0
+    var targetables: [Component.Type]
+    var interval: CGFloat
+    var directions: [Direction]
+    var speed: CGFloat
+
+    init(targetables: [Component.Type],
+         interval: CGFloat,
+         directions: [Direction],
+         speed: CGFloat = PeriodicallyShootPelletAttackStyle.DEFAULT_PELLET_SPEED) {
+        self.targetables = targetables
+        self.interval = interval
+        self.directions = directions
+        self.speed = speed
+    }
+
+    func attack(damage: CGFloat, attacker: Entity, attackee: Entity, delegate: AttackableDelegate) {
+        guard attacker != attackee,
+              canAttack(attackee, with: targetables, using: delegate),
+              let shooterPhysicsBody = delegate.getComponent(of: PhysicsComponent.self, for: attacker) else {
+            return
+        }
+        for direction in directions {
+            let pelletVelocity = getVelocity(direction: direction, speed: speed)
+            let createPelletEvent = CreateProjectileEvent(projectileType: .pellet,
+                                                          position: shooterPhysicsBody.physicsBody.position,
+                                                          velocity: pelletVelocity)
+            EventManager.shared.postEvent(createPelletEvent)
+        }
+    }
+
+    private func getVelocity(direction: Direction, speed: CGFloat) -> CGVector {
+        switch direction {
+        case .up:
+            return CGVector(dx: 0, dy: -speed)
+        case .down:
+            return CGVector(dx: 0, dy: speed)
+        case .left:
+            return CGVector(dx: -speed, dy: 0)
+        case .right:
+            return CGVector(dx: 0, dy: speed)
+        }
+    }
+}
