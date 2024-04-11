a858e9c269e82e9f654c0b1a69888cc47d6cc147
JCSnap <e0970167@u.nus.edu>
2024-04-11 18:21:58 +0800

    remove left right movement for ground block

--- TheAlienThatEatsTheCarrot/GameEngine/Entities/Factories/BlockFactories/GroundBlockFactory.swift
+++ TheAlienThatEatsTheCarrot/GameEngine/Entities/Factories/BlockFactories/GroundBlockFactory.swift
@@ -21,7 +21,6 @@ class GroundBlockFactory: BlockFactory {
                                                       frictionalStrength: GroundBlockFactory.FRICTIONAL_STRENGTH)
         let physicsBody = PhysicsBody(shape: .rectangle, position: boardObject.position, size: size, categoryBitmask: Constants.blockCategoryBitmask, collisionBitmask: Constants.blockCollisionBitmask, isDynamic: false)
         let physicsComponent = PhysicsComponent(entity: entity, physicsBody: physicsBody)
-        let movableComponent = MovableComponent(entity: entity, pattern: LeftRightPattern())
-        return [blockComponent, renderableComponent, frictionalComponent, physicsComponent, movableComponent]
+        return [blockComponent, renderableComponent, frictionalComponent, physicsComponent]
     }
 }
