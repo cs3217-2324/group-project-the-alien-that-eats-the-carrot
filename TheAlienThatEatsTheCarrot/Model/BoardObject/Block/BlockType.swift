0b311d1f338683089077dafb6304f42bb74426df
JCSnap <e0970167@u.nus.edu>
2024-04-11 14:46:06 +0800

    add temporary block type

--- TheAlienThatEatsTheCarrot/Model/BoardObject/Block/BlockType.swift
+++ TheAlienThatEatsTheCarrot/Model/BoardObject/Block/BlockType.swift
@@ -8,7 +8,7 @@
 import Foundation
 
 public enum BlockType: String {
-    case normal, ground, spike, breakable, pushable, mushroom, roller,
+    case normal, ground, spike, breakable, pushable, mushroom, roller, temporary,
          doubleJumpPowerup, strengthPowerup, attackPowerup, invinciblePowerup
 
     static let typeToAssetNameMap = [normal: "land-top",
@@ -18,6 +18,7 @@ public enum BlockType: String {
                                      pushable: "block-pushable",
                                      mushroom: "mushroom-1",
                                      roller: "platform-oneway",
+                                     temporary: "platform-solid",
                                      doubleJumpPowerup: "powerup-unused-blue",
                                      strengthPowerup: "power-unused-yellow",
                                      attackPowerup: "powerup-unused-red",
@@ -28,6 +29,7 @@ public enum BlockType: String {
                                 breakable: CGSize(width: 50, height: 50),
                                 mushroom: CGSize(width: 30, height: 30),
                                 roller: CGSize(width: 50, height: 1),
+                                temporary: CGSize(width: 50, height: 50),
                                 pushable: CGSize(width: 50, height: 50),
                                 doubleJumpPowerup: CGSize(width: 50, height: 50),
                                 strengthPowerup: CGSize(width: 50, height: 50),
