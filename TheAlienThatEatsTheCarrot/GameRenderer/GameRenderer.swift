07567e0c6e4b7f213e6995fb8e1f36af373b84f9
JCSnap <e0970167@u.nus.edu>
2024-04-19 17:55:14 +0800

    refactor get position offsets to game renderer

--- TheAlienThatEatsTheCarrot/GameRenderer/GameRenderer.swift
+++ TheAlienThatEatsTheCarrot/GameRenderer/GameRenderer.swift
@@ -8,4 +8,28 @@
 import Foundation
 
 class GameRenderer {
+    var scale: CGFloat
+    var unitSize: CGFloat
+    var boardAreaFrame: CGRect
+
+    init(scale: CGFloat, unitSize: CGFloat, boardAreaFrame: CGRect) {
+        self.scale = scale
+        self.unitSize = unitSize
+        self.boardAreaFrame = boardAreaFrame
+    }
+
+    func getPositionWithOffsets(for originalPosition: CGPoint, playerPosition: CGPoint?) -> CGPoint {
+        let verticalCameraOffset = 1_300.0
+        var xPosition = originalPosition.x + boardAreaFrame.width / scale / 2
+        var yPosition = originalPosition.y + boardAreaFrame.height / scale / 2 + verticalCameraOffset / scale
+        if let playerPosition = playerPosition {
+            xPosition -= playerPosition.x
+            yPosition -= playerPosition.y
+        }
+        return toBoardPosition(position: CGPoint(x: xPosition, y: yPosition))
+    }
+
+    private func toBoardPosition(position: CGPoint) -> CGPoint {
+        CGPoint(x: position.x * unitSize, y: position.y * unitSize)
+    }
 }
